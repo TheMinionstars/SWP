8ca8312cb3be7ace713b1c6653f9a9ae7569ea87
oixankcute <minhchauxauxi1234@gmail.com>
2023-06-20 00:23:50 +0700

    account

--- hotelmanagement/src/main/java/swp/server/hotelmanagement/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package swp.server.hotelmanagement.service;
-
-import swp.server.hotelmanagement.dtos.AccountDTO;
-
-import java.util.List;
-
-public interface AccountService {
-    List<AccountDTO> getAllAccounts();
-}
