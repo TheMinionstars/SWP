440c9d8e9d3c099d0a03938f5dfb395610ffd3ae
oixankcute <minhchauxauxi1234@gmail.com>
2023-07-08 03:09:59 +0700

    update

--- hotelmanagement/src/main/java/swp/server/hotelmanagement/controllers/AccountController.java
+++ hotelmanagement/src/main/java/swp/server/hotelmanagement/controllers/AccountController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("hotel-server/api/v1")
 @AllArgsConstructor
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class AccountController {
     private  final AccountService accountService;
     private final ProfileService profileService;
---
+++ hotelmanagement/src/main/java/swp/server/hotelmanagement/jwts/JwtUtils.java
@@ -0,0 +1,18 @@
+package swp.server.hotelmanagement.jwts;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JwtUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+    @Value("${jwtSecret}")
+    private String jwtSecret;
+    @Value("${jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+
+}
